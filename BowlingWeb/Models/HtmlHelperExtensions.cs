806427cdd9652218b8e1d2a9dbf11060b81dfc19
Donald <lugenius2001@gmail.com>
2022-12-14 16:29:40 +0800

    新增HtmlHelperExtensions, debug時到正確的Script、繪製Charts

---
+++ BowlingWeb/Models/HtmlHelperExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BowlingWeb.Models
+{
+    public static class HtmlHelperExtensions
+    {
+        public static HtmlString Versioned(this UrlHelper helper, string target)
+        {
+            if (!HttpContext.Current.IsDebuggingEnabled)
+            {
+                var minTarget = target.Substring(0, target.Length - 2) + "min.js";
+
+                if (File.Exists(HttpContext.Current.Server.MapPath(minTarget)))
+                {
+                    target = minTarget;
+                }
+
+                var file = HttpContext.Current.Server.MapPath(target);
+                DateTime lastModifiedDate = File.GetLastWriteTime(file);
+                string versionedUrl = $"{target}?v={lastModifiedDate.Ticks}";
+
+                return new HtmlString(helper.Content(versionedUrl));
+            }
+
+            return new HtmlString(helper.Content(target));
+        }
+    }
+}
--- BowlingWeb/Scripts/WebApp/Member/RecordCtrl.js
+++ BowlingWeb/Scripts/WebApp/Member/RecordCtrl.js
@@ -128,4 +128,25 @@ app.controller('ChartRecordCtrl', ['$scope', '$window', 'appService', '$rootScop
 
     $scope.Member = myFactory.get(); // 選擇要評分的成員資料
 
+    // 繪圖
+    const ctx = document.getElementById('myChart');
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
 }]);
